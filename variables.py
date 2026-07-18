cc0e165a2403b7c538631c6e0bbfb072dc2b07de
Sagaya Priyanka <priyarosariomary23@gmail.com>
2026-07-18 08:47:38 +0530

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,26 @@
+#Identifier &variable Ass
+ab_c='ravi'
+print (ab_c)
+
+#variable
+n=int(input())
+print(n)
+
+#Integer
+n=143
+print(type(n))
+
+#Float
+n=34.5
+print(type(n))
+
+#string
+n='ravi priya'
+print(type(n))
+
+#formatting variable
+n=56
+m=43
+print("{1}\n{0}".format(n,m))
+
+
